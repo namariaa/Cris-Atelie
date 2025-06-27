421986935de953ed0eb929d1ff561b00cc962d00
namariaa <anamariafag18@gmail.com>
2025-06-26 23:22:36 -0300

    feat: Finaliza funcionalidade CRUD

--- crisAtelie/src/app/components/card/card.components.ts
+++ crisAtelie/src/app/components/card/card.components.ts
@@ -3,10 +3,12 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { IProdutos } from '../../interfaces/IProdutos.interface';
 import { ProdutosList } from '../../banco';
+import { PopconfirmComponent } from '../popconfirm/popconfirm.component';
+import { ModalComponent } from '../modal/modal.components';
 
 @Component({
   selector: 'app-card',
-  imports: [NzCardModule, NzIconModule],
+  imports: [NzCardModule, NzIconModule, PopconfirmComponent, ModalComponent],
   standalone: true, 
   templateUrl: './card.components.html',
   styleUrl: './card.components.css'
--- crisAtelie/src/app/components/modal/modal.components.ts
+++ crisAtelie/src/app/components/modal/modal.components.ts
@@ -1,16 +1,16 @@
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzButtonModule, NzButtonType } from 'ng-zorro-antd/button';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { Component, inject, Input } from '@angular/core';
 import {
   FormsModule,
   NgForm,
-  NonNullableFormBuilder,
-  Validators,
 } from '@angular/forms';
-import { NzFormLayoutType, NzFormModule } from 'ng-zorro-antd/form';
+import {  NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { ButtonComponent } from '../button/button.component';
 import { ProdutosList } from '../../banco';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IProdutos } from '../../interfaces/IProdutos.interface';
 
 @Component({
   selector: 'app-modal',
@@ -22,12 +22,20 @@ import { ProdutosList } from '../../banco';
     NzInputModule,
     NzFormModule,
     ButtonComponent,
+    NzIconModule
   ],
   templateUrl: './modal.components.html',
   styleUrl: './modal.components.css',
 })
 export class ModalComponent {
   @Input() titulo!: string;
+  @Input() label?: string;
+  @Input() default?: IProdutos;
+  @Input() tipo!: string;
+  @Input() buttonStyle: NzButtonType = "primary";
+  @Input() buttonIcon: string = '';
+  @Input() id!: number;
+
   isVisible = false;
 
   showModal(): void {
@@ -35,12 +43,27 @@ export class ModalComponent {
   }
 
   handleOk(form: NgForm): void {
-    ProdutosList.push({
-      id: ProdutosList.length,
-      name: form.value['nome'],
-      description: form.value['descricao'],
-      value: form.value['valor'],
-    });
+    if (this.tipo == "criar"){
+      ProdutosList.push({
+        id: ProdutosList.length,
+        name: form.value['nome'],
+        description: form.value['descricao'],
+        value: form.value['valor'],
+      });
+    }
+    else{
+      console.log(form.value, this.id);
+      
+      if (form.value['nome']){
+        ProdutosList[this.id - 1].name = form.value['nome'];
+      }
+      if (form.value['descricao']){
+        ProdutosList[this.id - 1].description = form.value['descricao'];
+      }
+      if (form.value['valor']){
+        ProdutosList[this.id - 1].value = form.value['valor'];
+      }
+    }
 
     this.isVisible = false;
   }
