b9d65c682c9f30fc74950565654e415a73a85180
namariaa <anamariafag18@gmail.com>
2025-07-17 00:32:21 -0300

    feat: Insere rotas nos arquivos

--- crisAtelie/src/app/app.routes.ts
+++ crisAtelie/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.components';
+import { DetalharComponent } from './pages/Produto/detalhar/detalhar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: ":id",
+        component: DetalharComponent
+    }
+];
---
+++ crisAtelie/src/app/pages/Produto/detalhar/detalhar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhar',
+  imports: [],
+  templateUrl: './detalhar.component.html',
+  styleUrl: './detalhar.component.css'
+})
+export class DetalharComponent {
+
+}
